1c7f92f2a3a827f3e6f2f2a0fc248057553b8cc2
trttkm <trttkm@gmail.com>
2021-07-20 15:43:17 +0300

    добавление деструктуризации в классовом компоненте по рекоммендации преподавателя

--- src/components/ShopItemClass.js
+++ src/components/ShopItemClass.js
@@ -7,18 +7,20 @@ class ShopItemClass extends React.Component {
   };
   
   render() {
+    const { item } = this.props;
+    
     return (
       <div className="main-content">
-        <h2>{this.props.item.brand}</h2>
-        <h1>{this.props.item.title}</h1>
-        <h3>{this.props.item.description}</h3>
-        <div className="description">{this.props.item.descriptionFull}</div>
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
         <div className="highlight-window mobile">
           <div className="highlight-overlay" />
         </div>
         <div className="divider" />
         <div className="purchase-info">
-          <div className="price">{this.props.item.currency}{this.props.item.price.toFixed(2)}</div>
+          <div className="price">{item.currency}{item.price.toFixed(2)}</div>
           <button>Добавить в корзину</button>
         </div>
       </div>
